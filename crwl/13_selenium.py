1eb1a2e21528ce93bcdf5720b208d60dc35348bc
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2021-12-07 20:12:44 +0900

    Update 13_selenium.py

--- crwl/13_selenium.py
+++ crwl/13_selenium.py
@@ -1,28 +1,40 @@
-import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import pyperclip
 
 browser = webdriver.Chrome() # "./chromedriver.exe"
 
 # 1. 네이버 이동
-browser.get("http://naver.com")
+browser.get("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com")
+uid = 'gkraud4135'
+upw = 'pwpw'
 
-# 2. 로그인 버튼 클릭
-elem = browser.find_element_by_class_name("link_login")
-elem.click()
+time.sleep(2) #로딩 대기
 
-# 3. id, pw 입력
-browser.find_element_by_id("id").send_keys("naver_id")
-browser.find_element_by_id("pw").send_keys("password")
+tag_id = browser.find_element_by_id('id')
+tag_pw = browser.find_element_by_id('pw')
 
-# 4. 로그인 버튼 클릭
-browser.find_element_by_id("log.login").click()
+# id 입력
+tag_id.click()
+pyperclip.copy(uid)
+tag_id.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+# pw 입력
+tag_pw.click()
+pyperclip.copy(upw)
+tag_pw.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
 
-time.sleep(3)
+#로그인 버튼 클릭
+login_btn = browser.find_element_by_id('log.login')
+login_btn.click()
+time.sleep(2)
 
 # 5. id 를 새로 입력
 #browser.find_element_by_id("id").send_keys("my_id")
-browser.find_element_by_id("id").clear()
-browser.find_element_by_id("id").send_keys("my_id")
+#browser.find_element_by_id("id").clear()
+#browser.find_element_by_id("id").send_keys("my_id")
 
 # 6. html 정보 출력
 print(browser.page_source)
